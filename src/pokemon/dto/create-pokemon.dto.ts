863daa4c39e7e4a331d498594acf5230dded4fc7
KatriEsFa <katriesfa@gmail.com>
2023-02-15 23:04:26 -0300

    first commit

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,14 @@
+import { MinLength, IsInt, IsPositive, Min, IsString } from 'class-validator';
+
+export class CreatePokemonDto {
+  // isInt, isPositive, min 1
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  //isString, minLength 1
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
